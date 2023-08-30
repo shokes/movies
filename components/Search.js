2e96bd3903adc058130e7dfc45f28719cea2a3b8
osezei <john.osezei@yahoo.com>
2023-08-30 10:16:40 +0100

    home

--- components/Search.js
+++ components/Search.js
@@ -18,11 +18,11 @@ const Search = () => {
   }
 
   return (
-    <section className="flex justify-between">
+    <section className="">
       {/* buttons */}
-      <div>
-        <div>
-          <ul>
+      <div className="flex gap-x-9">
+        <div className="bg-[#161D2F] h-[960px] mt-8 ml-8 rounded-lg ">
+          <ul className="cursor-pointer ">
             {["all", ...new Set(category)].map((item, index) => {
               return (
                 <li
@@ -40,44 +40,55 @@ const Search = () => {
         </div>
         {/* end of buttons */}
 
-        {/* search form */}
-        <form className="flex">
-          <p className="my-auto text-white pr-6 text-2xl">
-            <BiSearch />
-          </p>
-          <input
-            index="searchInput"
-            type="text"
-            placeholder="Search for movies or TV series..."
-            onChange={(event) => {
-              setSearchTerm(event.target.value);
-            }}
-            className="text-white bg-transparent"
-          />
-        </form>
-        {/* search form end */}
+        <div className="mt-[65px]">
+          {/* search form */}
 
-        {/* movie list */}
-        <div className="grid grid-cols-4">
-          {movie_list
-            .filter((val) => {
-              if (searchTerm === "") {
-                return val;
-              } else if (
-                val.title.toLowerCase().includes(searchTerm.toLowerCase())
-              ) {
-                return val;
-              }
-            })
-            .map((val, index) => {
-              if (val.isTrending === true) {
-                return <Trending key={index} {...val} />;
-              } else {
-                return <List key={index} {...val} />;
-              }
-            })}
+          <form className="flex mb-[35px]">
+            <p className="my-auto text-white pr-6 text-2xl">
+              <BiSearch />
+            </p>
+            <input
+              index="searchInput"
+              type="text"
+              placeholder="Search for movies or TV series"
+              onChange={(event) => {
+                setSearchTerm(event.target.value);
+              }}
+              className="text-white bg-transparent w-[400px] focus:outline-none"
+            />
+          </form>
+          {/* search form end */}
+
+          {/* movie list */}
+          <div className="flex flex-wrap gap-x-10">
+            {movie_list
+              .filter((val) => {
+                if (searchTerm === "") {
+                  return val;
+                } else if (
+                  val.title.toLowerCase().includes(searchTerm.toLowerCase())
+                ) {
+                  return val;
+                }
+              })
+
+              .map((val, index) => {
+                if (val.isTrending === true && movie_list.length === 29) {
+                  return (
+                    <>
+                      {" "}
+                      <div className="">
+                        <Trending key={index} {...val} />
+                      </div>
+                    </>
+                  );
+                } else {
+                  return <List key={index} {...val} />;
+                }
+              })}
+          </div>
+          {/* movie list end */}
         </div>
-        {/* movie list end */}
       </div>
     </section>
   );
